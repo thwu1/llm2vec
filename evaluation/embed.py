87eab2891c8f7c49afed4163a95a1f476ee5eb4b
jiaolab-tianhao <wth569749099@gmail.com>
2024-02-05 00:40:25 +0000

    refactor

--- evaluation/embed.py
+++ evaluation/embed.py
@@ -1,32 +1,49 @@
 from sentence_transformers import SentenceTransformer
 import json
 import os
+import argparse
+from tqdm import tqdm
 
-embedder_name = "all-mpnet-base-v2"
-embedder = SentenceTransformer(embedder_name)
+if __name__ == "__main__":
 
-file_paths = os.listdir("evaluation/outputs")
-filtered_file_paths = [f"evaluation/outputs/{fp}" for fp in file_paths if "embed" not in fp and f"{fp[:-5]}_{embedder_name}_embed.json" not in file_paths]
-print(filtered_file_paths)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--embedder", type=str, default="all-mpnet-base-v2")
+    args = parser.parse_args()
 
-for file_path in filtered_file_paths:
-    json_data = json.load(open(file_path, "r"))
+    embedder_name = args.embedder
+    embedder = SentenceTransformer(embedder_name)
 
-    def get_sentences(data):
-        ls = []
-        for item in data:
-            for output in item["output"]:
-                ls.append(item["prompt"] + output)
-        return ls
+    file_paths = os.listdir("evaluation/outputs")
+    embed_file_paths = os.listdir(f"evaluation/outputs/{embedder_name}")
+    if not os.path.exists(f"evaluation/outputs/{embedder_name}"):
+        os.makedirs(f"evaluation/outputs/{embedder_name}")
+    filtered_file_paths = [
+        f"evaluation/outputs/{fp}" for fp in file_paths if fp.endswith(".json") and f"{fp[:-5]}_embed.json" not in embed_file_paths
+    ]
+    print("Files to be embedded:", filtered_file_paths)
 
-    sentences = get_sentences(json_data)
-    sentence_embeddings = embedder.encode(sentences)
+    for file_path in tqdm(filtered_file_paths):
+        json_data = json.load(open(file_path, "r"))
 
-    idx = 0
-    for item in json_data:
-        item["embedding"] = sentence_embeddings[idx : idx + len(item["output"])].tolist()
-        idx += len(item["output"])
+        def get_sentences(data):
+            ls = []
+            for item in data:
+                for output in item["output"]:
+                    ls.append(item["prompt"] + output)
+            return ls
 
-    assert idx == len(sentence_embeddings)
+        sentences = get_sentences(json_data)
+        sentence_embeddings = embedder.encode(sentences)
 
-    json.dump(json_data, open(file_path.replace(".json", f"_{embedder_name}_embed.json"), "w"), indent=2)
+        idx = 0
+        for item in json_data:
+            item["embedding"] = sentence_embeddings[idx : idx + len(item["output"])].tolist()
+            idx += len(item["output"])
+
+        assert idx == len(sentence_embeddings)
+
+        json.dump(
+            json_data,
+            open(f"evaluation/outputs/{embedder_name}/" + file_path.split("/")[-1].replace(".json", f"_embed.json"), "w"),
+            indent=2,
+        )
