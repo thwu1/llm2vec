fa1faa8143a858a61481c8dcb09e4a9f5fe1ee95
jiaolab-tianhao <wth569749099@gmail.com>
2024-04-20 05:44:45 +0000

    support large dataset

--- tune_knn.py
+++ tune_knn.py
@@ -5,7 +5,9 @@ import numpy as np
 
 train_ls, test_ls = get_train_test()
 
-def tune_knn(config):
+
+def tune_knn(config, data):
+    train_ls, test_ls = data[0], data[1]
     info = evaluate(train_ls, test_ls, num_neighbors=config["num_neighbors"])
     return info
 
@@ -14,12 +16,9 @@ search_space = {"num_neighbors": tune.sample_from(lambda _: np.random.randint(1,
 tune_knn_with_resources = tune.with_resources(tune_knn, {"cpu": 1})
 
 tuner = tune.Tuner(
-    tune_knn_with_resources,
+    tune.with_parameters(tune_knn_with_resources, data=[train_ls, test_ls]),
     param_space=search_space,
-    tune_config=tune.TuneConfig(
-        num_samples=4,
-        scheduler=ASHAScheduler(metric="mean_accuracy", mode="max")
-    ),
+    tune_config=tune.TuneConfig(num_samples=4, scheduler=ASHAScheduler(metric="mean_accuracy", mode="max")),
 )
 
 results = tuner.fit()
